9db793a98dfc4e8885bbef8caf865a42d128a2ab
Juan Pablo Salas Clavijo <111824232+JuanPaSc@users.noreply.github.com>
2024-03-24 19:34:30 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,22 +17,24 @@ def create_dataset(data_dir):
     data = []
     for sentiment in os.listdir(data_dir):
         sentiment_dir = os.path.join(data_dir, sentiment)
+
         # Ignorar archivos ocultos, como .DS_Store en macOS
         if not sentiment.startswith("."):
             for filename in os.listdir(sentiment_dir):
                 filepath = os.path.join(sentiment_dir, filename)
+
                 # Ignorar archivos ocultos
                 if not filename.startswith("."):
                     with open(filepath, "r", encoding="utf-8") as file:
                         phrase = file.read().strip()
                     data.append({"phrase": phrase, "sentiment": sentiment})
+
     return pd.DataFrame(data)
 
 
-# Crear el conjunto de datos de entrenamiento
+# Crear csv
 train_data = create_dataset("train")
 train_data.to_csv("train_dataset.csv", index=False)
 
-# Crear el conjunto de datos de prueba
 test_data = create_dataset("test")
 test_data.to_csv("test_dataset.csv", index=False)
